c7718c2015a75c5d568bfe4d1d0070c8ff9221d8
Desopilante <pe.estevez92@gmail.com>
2020-03-10 23:14:07 -0300

    first upload

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,21 @@
+<template>
+	<b-jumbotron>
+		<template v-slot:lead>
+			{{ currentQuestion.question}}
+		</template>
+		<hr class="my-4">
+		<p>
+			List of answers
+		</p>
+		<b-button variant="primary" href="#">Submit</b-button>
+		<b-button variant="success" href="#">Next</b-button>
+	</b-jumbotron>
+</template>
+
+<script>
+	export default {
+		props: {
+			currentQuestion: Object
+		}
+	}
+</script>
